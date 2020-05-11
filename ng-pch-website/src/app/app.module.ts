67a5068bc2431ac9815ad8f823346be030168b05
Po-Chien Hao <pochienh@tw.ibm.com>
2020-05-11 18:40:18 +0800

    Google Angular Tutorial - Heroes

--- ng-pch-website/src/app/app.module.ts
+++ ng-pch-website/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
