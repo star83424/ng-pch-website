3dbee3ea2988a43480c60b1c3200ed804f3c72b0
Po-Chien Hao <pochienh@tw.ibm.com>
2020-05-18 11:31:18 +0800

    Delete unused spec.ts

--- ng-pch-website/src/app/age-impure.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AgeImpurePipe } from './age-impure.pipe';
-
-describe('AgeImpurePipe', () => {
-  it('create an instance', () => {
-    const pipe = new AgeImpurePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
